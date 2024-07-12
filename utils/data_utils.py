49ef6d645bdabb9475eadac8ba7adf1766d034bf
backofficerecadastro <backoffice.recadastro@gmail.com>
2024-07-12 19:11:18 -0300

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,63 +1,67 @@
-import streamlit as st
 import pandas as pd
+import numpy as np
+from glob import glob
+import streamlit as st
 import plotly.express as px
-from utils.data_utils import load_and_concatenate_data, preprocess_data, create_bar_chart, create_line_chart
+import plotly.io as pio
 
-# Configuração da Página
-st.set_page_config(layout="wide")
 
-def main():
-        
-    st.title("Dashboard de Visitas por dia")
-
-    # Carregar dados
-    df = load_and_concatenate_data()
-
-    # Pré-processar dados
-    df = preprocess_data(df)
+@st.cache_data
+def load_and_concatenate_data(file_pattern='datasets/*.csv', sep=';'):
+    # Encontrar arquivos correspondentes ao padrão
+    files = glob(file_pattern)
+    if not files:
+        raise ValueError(f"No files found for the pattern {file_pattern}")
     
-    # Configuração de Filtro
-    l0 = list(df['Localidade'].unique())
-    l0.sort()
-    local = ['TODAS'] + l0
-    m0 = list(df['Month'].unique())
-    m0.sort(reverse=True)
-    localidade = st.sidebar.selectbox('Localidade', local)
-    ano = st.sidebar.selectbox('Ano', df['Year'].unique())
-    mes = st.sidebar.selectbox('Mês', m0)
+    # Carregar e concatenar os arquivos
+    dataframes = [pd.read_csv(file, sep=sep, low_memory=False) for file in files]
+    return pd.concat(dataframes, ignore_index=True)
 
-    if localidade == 'TODAS':
-        filtro = local
-    else:
-        filtro = [localidade]
+@st.cache_data
+def preprocess_data(df):
+    df['Tipo de Validação'] = df['Tipo de Validação'].fillna('Não Validado')
+    df['Data da Validação'] = df['Data da Validação'].fillna("01/01/1900 00:00:00")
+    df = df.fillna(0)
+    df['Data do Cadastro'] = pd.to_datetime(df['Data do Cadastro'], dayfirst=True, format='mixed')
+    df['Data da Validação'] = pd.to_datetime(df['Data da Validação'], dayfirst=True, format='mixed')
+    df['dt_validação'] = df['Data da Validação'].dt.date
+    df = df.sort_values(['Data da Validação', 'Data do Cadastro'], ascending=False)
+    df['Year'] = df['Data do Cadastro'].dt.year.astype(str)
+    df['Month'] = df['Data do Cadastro'].dt.month.astype(str)
+    df['Day'] = df['Data do Cadastro'].dt.strftime('%d')
+    df['Hour'] = df['Data do Cadastro'].dt.hour.astype(str)
+    df['Ano de Validação'] = df['Data da Validação'].dt.year.astype(str)
+    df['Mês de Validação'] = df['Data da Validação'].dt.month.astype(str)
+    nao_validavel = ['MORADOR IMPEDIU', 'CASA FECHADA 1ª VISITA', 'CASA FECHADA 2ª VISITA', 'casa fechada']
+    df['Tipo de Ocorrência'] = df['Ocorrências'].apply(lambda x: 'Não Validável' if x in nao_validavel else 'Validável')
+    return df
 
-    df_filtrado = df[(df['Year'] == ano) & (df['Month'] == mes)]
-    df_filtrado = df_filtrado[df_filtrado['Localidade'].isin(filtro)]
-    df_filtrado = df_filtrado.sort_values(['Data do Cadastro'], ascending=False)
+@st.cache_data
+def create_bar_chart(data, x, y, color, title, orientation='v', color_map=None, color_map1=None, text_auto=True):
+    fig = px.bar(data, x=x, y=y, color=color, title=title, text_auto=text_auto, color_discrete_map=color_map, color_discrete_sequence=color_map1, orientation=orientation)
+    fig.update_layout(legend=dict(
+        orientation='h',
+        xanchor='center',
+        yanchor='bottom',
+        x=0.5,
+        y=-0.5
+    ))
+    return fig
 
-    df_validação = df[(df['Ano de Validação'] == ano) & (df['Month'] == mes) & (df['Tipo de Validação'] == 'Validado')]
-    df_validação = df_validação[df_validação['Localidade'].isin(filtro)]
-
-    # Gráficos
-    color_map = {'Validado': '#00049E', 'Não Validado': '#7275FE', 'Aguardando Análise': 'gray', 
-                 'Complemento de Fotos e Informações': 'gray', 'Revisão Interna': 'gray',
-                 'Validável': '#00049E', 'Não Validável': 'gray'}
-    color_map1 = ['#00049E','#7275FE']
-
-    prod_dia = df_filtrado.groupby(['Day', 'Tipo de Ocorrência'])['Matrícula'].count().reset_index()
-    fig_date = create_line_chart(prod_dia, 'Day', 'Matrícula', 'Tipo de Ocorrência', "Visitas por dia", color_map=color_map)
-    st.plotly_chart(fig_date)
-
-    Agente_dia = df_filtrado.groupby(df_filtrado['Day'])['Agente do Cadastro'].nunique().reset_index()
-    fig_agente = create_line_chart(Agente_dia, 'Day', 'Agente do Cadastro', None, "N° de agentes por dia", color_map1=color_map1)
-    st.plotly_chart(fig_agente)
-
-    pivot_table = pd.pivot_table(df_filtrado, values='Matrícula', index=['Localidade','Agente do Cadastro'], columns='Day', aggfunc='count', fill_value=0)
-    st.markdown("<h3 style='font-size:16px;'> Produção de Agentes por dia </h3>", unsafe_allow_html=True)
-    st.dataframe(pivot_table)
-    st.write("")
-
-if __name__ == "__main__":
-    main()
-
+@st.cache_data
+def create_line_chart(data, x, y, color, title, color_map=None, color_map1=None):
+    fig = px.line(data, x=x, y=y, color=color, title=title, color_discrete_map=color_map, color_discrete_sequence=color_map1, markers=True, text=y, line_shape='spline')
+    fig.update_layout(legend=dict(
+        orientation='h',
+        xanchor='center',
+        yanchor='bottom',
+        x=0.5,
+        y=-0.5
+    ))
+    fig.update_layout(
+    xaxis=dict(
+        tickmode='linear',
+        tickangle=0  # Ajusta o ângulo dos rótulos para melhorar a legibilidade
+    ))
+    return fig
 
